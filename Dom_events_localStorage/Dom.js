ee88634bc2c54b1e9336dab30bea4e7764a1f17e
Elechia Juma <otienoelechia3@gmail.com>
2025-02-13 04:18:21 +0300

    Javascript file practice on dom

---
+++ Dom_events_localStorage/Dom.js
@@ -0,0 +1,37 @@
+// changing text content
+// const myLady = document.getElementById("demo")
+// myLady.textContent ="Text changed succesfully"
+
+// change text content using button
+// function changeText() {
+//     document.getElementById("demo"). textContent = "Hello world !"
+// }
+
+//  Modify styles dynamically to change background and text color
+// function changeBackColor() {
+//     document.getElementById("divEl").style.backgroundColor = "brown"
+// }
+// function changeTextColor() {
+//     document.getElementById("divEl").style.color = "white"
+// }
+
+// Create and remove elements , add and remove elements dynamically from a list
+// function addItem() {
+//  let input = document.getElementById("itemInput");
+//  let list = document.getElementById("itemList");
+
+//  if (input.value.trim() !== ""){
+//     let li = document.createElement("li");
+//     li.textContent = input.value;
+//     let deleteBtn = document.createElement("button");
+//     deleteBtn.textContent = "X"
+//     deleteBtn.onclick = function () {
+//         list.removeChild(li)
+//     }
+//     li.appendChild(deleteBtn);
+//     list.appendChild(li);
+//     input.value = ""
+//  }
+
+// }
+
